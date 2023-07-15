ba65606f0e59294293c7c55e396037f1fdd487d9
Jack Geng <103839282+jackgeng19@users.noreply.github.com>
2023-07-15 17:58:01 -0400

    greater than 60

--- matplot.py
+++ matplot.py
@@ -16,12 +16,15 @@ def generate_plot(PATH, TITLE, X_LABEL, Y_LABEL):
                 next(reader)  # Skip the header row
                 for row in reader:
                     duration = int(row[1])
-                    durations.append(duration)
+                    if duration >= 60:
+                        durations.append(duration)
 
-    filtered_durations = [duration for duration in durations if duration <= 60]
-
+    if len(durations) >= 2:
+        durations.sort(reverse=True)
+        second_max_duration = durations[1]
+        print("Second maximum duration:", second_max_duration)
     # Set custom color and edgecolor
-    plt.hist(filtered_durations, bins=range(10, 61, 1), edgecolor='white', color='#1f77b4', alpha=0.8, rwidth=0.8)
+    plt.hist(durations, bins=range(60, 1250, 10), edgecolor='None', color='#1f77b4', alpha=0.8, rwidth=0.8)
     plt.title(TITLE, fontweight='bold')
     plt.xlabel(X_LABEL)
     plt.ylabel(Y_LABEL)
@@ -31,13 +34,13 @@ def generate_plot(PATH, TITLE, X_LABEL, Y_LABEL):
     plt.gca().set_facecolor('#f2f2f2')
 
 
-    counts, bins, _ = plt.hist(filtered_durations, bins=range(10, 61, 1), edgecolor='white', color='#1f77b4', alpha=0.8, rwidth=0.8)
-    top_counts_indices = (-counts).argsort()[:2]  # Get indices of top two counts
-    for index in top_counts_indices:
-        count = int(counts[index])
-        bin_value = bins[index] + 0.5
-        plt.annotate(f'{count}', xy=(bin_value, count), xytext=(0, 10), textcoords='offset points',
-                     ha='center', va='bottom')
+    # counts, bins, _ = plt.hist(filtered_durations, bins=range(10, 61, 1), edgecolor='white', color='#1f77b4', alpha=0.8, rwidth=0.8)
+    # top_counts_indices = (-counts).argsort()[:2]  # Get indices of top two counts
+    # for index in top_counts_indices:
+    #     count = int(counts[index])
+    #     bin_value = bins[index] + 0.5
+    #     plt.annotate(f'{count}', xy=(bin_value, count), xytext=(0, 10), textcoords='offset points',
+    #                  ha='center', va='bottom')
         
     # Adjust spacing
     plt.tight_layout()
