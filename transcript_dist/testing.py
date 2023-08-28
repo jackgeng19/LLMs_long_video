a6a1f37115d6876331134ab6c06588167ddb83bf
Jack Geng <103839282+jackgeng19@users.noreply.github.com>
2023-08-28 11:39:03 -0400

    testing

--- transcript_dist/testing.py
+++ transcript_dist/testing.py
@@ -8,7 +8,9 @@ import numpy as np
 # Iterate over the range of file numbers (0000 to 1023) in batches
 file_number_str = "0000"
 
-input_file_name = f"./..shanw25/data/YT1B/metadata/yttemporal1b_train_{file_number_str}of1024.jsonl.gz"
+# input_file_name = f"./..shanw25/data/YT1B/metadata/yttemporal1b_train_{file_number_str}of1024.jsonl.gz"
+
+input_file_name = "/Users/jackgengqc/code/appteam/academy/VideoQA-Generation/transcript_dist/yttemporal1b_train_0000of1024.jsonl.gz"
 
 # Build the temporary unzipped file name
 unzipped_file_name = f"./yttemporal1b_train_{file_number_str}of1024.jsonl"
@@ -25,25 +27,30 @@ with open(unzipped_file_name, 'r') as json_file:
 
     # Open the CSV file for writing
     with open(output_file_name, 'w', newline='') as csv_file:
+        # Write the header row in the CSV file
         writer = csv.writer(csv_file)
 
-        # Write the header row in the CSV file
+        writer.writerow(['ID', 'Duration', 'Delta * 1000'])
 
         # Process each line in the JSONL file
         for line in json_file:
             data = json.loads(line)
-            subtitles_t_start = data["_subtitles_t_start"]
-            subtitles_t_end = data["_subtitles_t_end"]
+            id_value = data['id']
+            duration = data["duration"]
+
+            if duration < 600:
+                continue
+            subtitles_t_start = data["subtitles_t_start"]
+            subtitles_t_end = data["subtitles_t_end"]
             # Calculate time deltas between consecutive words
-            time_deltas = [end - start for start, end in zip(subtitles_t_start[:-1], subtitles_t_end[1:])]
+            time_deltas = [start - end for start, end in zip(subtitles_t_start[1:], subtitles_t_end[:-1])]
 
             # Calculate the mean of the time deltas
-            mean_time_delta = sum(time_deltas) / len(time_deltas)
+            mean_time_delta = np.mean(time_deltas)
 
-            # if mean_time_delta >= 20:
-            #     continue
 
-            print(mean_time_delta)
+
+            writer.writerow([id_value, duration, mean_time_delta * 1000])            
 
     os.remove(unzipped_file_name)
 
